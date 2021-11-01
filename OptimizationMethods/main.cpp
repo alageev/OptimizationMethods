eafcfcb7bcc4d99adee67de1a9726be3629a7091
alageev <alageev01@gmail.com>
2021-11-01 21:43:09 +0300

    main.cpp updated to call quadratic interpolation method

--- OptimizationMethods/main.cpp
+++ OptimizationMethods/main.cpp
@@ -7,32 +7,61 @@
 
 #include <iostream>
 #include <cmath>
+#include <ctime>
 #include "Functions.h"
 
 
 int main(int argc, const char * argv[]) {
-    const double halvesResult = halvesMethod(interval, epsilon);
-    std::cout << "halves method:       f(x) = " << function(halvesResult)      << ", x = " << halvesResult      << std::endl;
-    
-    const double goldenRatioResult = goldenRatioMethod(interval, epsilon);
-    std::cout << "golden ratio method: f(x) = " << function(goldenRatioResult) << ", x = " << goldenRatioResult << std::endl;
-    
+    {
+        auto startTime = std::chrono::high_resolution_clock::now();
+        const double halvesResult = halvesMethod(interval, epsilon);
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+        
+        std::cout << "halves method: " << function(halvesResult) << " at " << halvesResult;
+        std::cout << ", t = " << methodTime << std::endl;
+    }
+    {
+        auto startTime = std::chrono::high_resolution_clock::now();
+        const double goldenRatioResult = goldenRatioMethod(interval, epsilon);
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+        
+        std::cout << "golden ratio method: " << function(goldenRatioResult) << " at " << goldenRatioResult;
+        std::cout << ", t = " << methodTime << std::endl;
+    }
     try {
+        auto startTime = std::chrono::high_resolution_clock::now();
         const double chordesResult = chordesMethod(interval, epsilon);
-        std::cout << "chordes method:      f(x) = " << function(chordesResult)     << ", x = " << chordesResult     << std::endl;
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+        
+        std::cout << "chordes method: " << function(chordesResult) << " at " << chordesResult;
+        std::cout << ", t = " << methodTime << std::endl;
     } catch(const std::exception &exception) {
         std::cout << "chordes method: " << exception.what() << std::endl;
     }
     
     try {
+        auto startTime = std::chrono::high_resolution_clock::now();
         const double newtonResult = newtonMethod(interval, epsilon);
-        std::cout << "newton method:       f(x) = " << function(newtonResult)      << ", x = " << newtonResult      << std::endl;
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+        
+        std::cout << "newton method: " << function(newtonResult) << " at " << newtonResult;
+        std::cout << ", t = " << methodTime << std::endl;
     } catch(const std::exception &exception) {
         std::cout << "newton method: " << exception.what() << std::endl;
     }
-
-    const double interpolationResult = quadraticInterpolationMethod(interval, epsilon);
-    std::cout << "interpolation method:       f(x) = " << function(interpolationResult)      << ", x = " << interpolationResult      << std::endl;
+    {
+        auto startTime = std::chrono::high_resolution_clock::now();
+        const double interpolationResult = quadraticInterpolationMethod(interval, epsilon);
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+        
+        std::cout << "interpolation method: " << function(interpolationResult) << " at " << interpolationResult;
+        std::cout << ", t = " << methodTime << std::endl;
+    }
     
     return 0;
 }
