64e387541a2e1afd7bcd0d7f479a0ddab2dcc343
alageev <alageev01@gmail.com>
2021-11-02 19:52:30 +0300

    one-dimensional method was removed gradient descent method was added

--- OptimizationMethods/Functions.h
+++ OptimizationMethods/Functions.h
@@ -8,95 +8,37 @@
 #ifndef Functions_h
 #define Functions_h
 
-#include "Constants.h"
+#include "Vector.h"
 
-double function(double x) {
-    return x * x - 3 * x + x * log(x);
+
+double function(Vector vector) {
+    return vector.x * vector.x - vector.x * vector.y + vector.y * vector.y - 2 * vector.x + vector.y;
 }
 
-double firstDerivative(double x) {
-    return 2 * x - 3 + log(x) + x / x;
+double xDerivative(Vector vector) {
+    return 2 * vector.x - vector.y - 2;
 }
 
-double secondDerivative(double x) {
-    return 2 + 1 / x;
+double yDerivative(Vector vector) {
+    return -vector.x + 2 * vector.y + 1;
 }
 
-double halvesMethod(Interval interval, double epsilon) {
+Vector gradF(Vector vector) {
+    return Vector(xDerivative(vector), yDerivative(vector));
+}
+
+Vector gradientDescent(Vector vector) {
+    Vector minVector = vector;
+    Vector antigradient = -gradF(minVector);
     
-    if (interval.length() <= 2 * epsilon) {
-        return interval.middle();
+    while (antigradient.norm() >= epsilon) {
+        Vector newVector = minVector + antigradient * step;
+        minVector = newVector;
+        antigradient = -gradF(newVector);
     }
     
-    const double middle = interval.middle();
-    const double leftCoordinate  = middle - epsilon / 2;
-    const double rightCoordinate = middle + epsilon / 2;
-    
-    if (function(leftCoordinate) < function(rightCoordinate)) {
-        return halvesMethod(Interval(interval.start, rightCoordinate), epsilon);
-    } else {
-        return halvesMethod(Interval(leftCoordinate, interval.end), epsilon);
-    }
+    return minVector;
 }
 
-double goldenRatioMethod(Interval interval, double epsilon) {
-    
-    double leftCoordinate  = interval.start + (2 - phi) * interval.length();
-    double rightCoordinate = interval.start + (phi - 1) * interval.length();
-    
-    do {
-        if (function(leftCoordinate) < function(rightCoordinate)) {
-            interval.end = rightCoordinate;
-            rightCoordinate = leftCoordinate;
-            leftCoordinate = interval.start + (2 - phi) * interval.length();
-        } else {
-            interval.start = leftCoordinate;
-            leftCoordinate = rightCoordinate;
-            rightCoordinate = interval.start + (phi - 1) * interval.length();
-        }
-    } while (interval.length() > 2 * epsilon);
-    
-    return interval.middle();
-}
-
-double chordesMethod(Interval interval, double epsilon) {
-    
-    if (firstDerivative(interval.start) * firstDerivative(interval.end) >= 0) {
-        throw std::range_error("derivatives have equal sign on both ends of given interval");
-    }
-    
-    double intersection;
-    
-    do {
-        double difference = firstDerivative(interval.start) - firstDerivative(interval.end);
-        double ratio = firstDerivative(interval.start) / difference;
-        intersection = interval.start + ratio * interval.length();
-        
-        if (firstDerivative(intersection) < 0) {
-            interval.start = intersection;
-        } else {
-            interval.end = intersection;
-        }
-        
-    } while (std::abs(firstDerivative(intersection)) > epsilon);
-    
-    return intersection;
-}
-
-double newtonMethod(Interval interval, double epsilon) {
-    
-    double x = interval.random();
-    
-    while (std::abs(firstDerivative(x)) > epsilon) {
-        const double newX = x - firstDerivative(x) / secondDerivative(x);
-        x = newX;
-    }
-    
-    if (!interval.contains(x)) {
-        throw std::range_error("found x = " + std::to_string(x) + " is not in the interval");
-    }
-    
-    return x;
-}
-
+// vector vector vector vector
 #endif /* Functions_h */
