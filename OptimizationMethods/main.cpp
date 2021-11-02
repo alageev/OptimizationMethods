a3fc9957a601012c5eb97aa1c1c0db462189d243
alageev <alageev01@gmail.com>
2021-11-02 19:54:26 +0300

    one-dimensional methods were replaced by gradient descent

--- OptimizationMethods/main.cpp
+++ OptimizationMethods/main.cpp
@@ -6,29 +6,20 @@
 //
 
 #include <iostream>
-#include <cmath>
+#include "Constants.h"
 #include "Functions.h"
 
 
 int main(int argc, const char * argv[]) {
-    const double halvesResult = halvesMethod(interval, epsilon);
-    std::cout << "halves method:       f(x) = " << function(halvesResult)      << ", x = " << halvesResult      << std::endl;
     
-    const double goldenRatioResult = goldenRatioMethod(interval, epsilon);
-    std::cout << "golden ratio method: f(x) = " << function(goldenRatioResult) << ", x = " << goldenRatioResult << std::endl;
-    
-    try {
-        const double chordesResult = chordesMethod(interval, epsilon);
-        std::cout << "chordes method:      f(x) = " << function(chordesResult)     << ", x = " << chordesResult     << std::endl;
-    } catch(const std::exception &exception) {
-        std::cout << "chordes method: " << exception.what() << std::endl;
-    }
-    
-    try {
-        const double newtonResult = newtonMethod(interval, epsilon);
-        std::cout << "newton method:       f(x) = " << function(newtonResult)      << ", x = " << newtonResult      << std::endl;
-    } catch(const std::exception &exception) {
-        std::cout << "newton method: " << exception.what() << std::endl;
+    {
+        auto startTime = std::chrono::high_resolution_clock::now();
+        const Vector gradientResult = gradientDescent(initialVector);
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+        
+        std::cout << "gradient descent method: " << function(gradientResult) << " at " << gradientResult;
+        std::cout << ", t = " << methodTime << std::endl;
     }
     
     return 0;
