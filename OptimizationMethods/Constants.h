c783c25b0ee243c125c404be3543097eb92a9375
alageev <alageev01@gmail.com>
2021-12-14 11:04:09 +0300

    Constants updated

--- OptimizationMethods/Constants.h
+++ OptimizationMethods/Constants.h
@@ -11,12 +11,15 @@
 
 const double mutationProbability = 0.01;
 const int populationSize = 4;
-const double distances[5][5] = {
-    {0, 4, 5, 3, 5},
-    {4, 0, 7, 6, 8},
-    {5, 7, 0, 7, 9},
-    {3, 6, 7, 0, 9},
-    {8, 8, 9, 9, 0}
+const int numberOfCities = 5;
+const int idealLength = 31;
+const double distances[numberOfCities][numberOfCities] = {
+    {0, 4, 5, 3, 8}, // 0
+    {4, 0, 7, 6, 8}, // 1
+    {5, 7, 0, 7, 9}, // 2
+    {3, 6, 7, 0, 9}, // 3
+    {8, 8, 9, 9, 0}  // 4
+//   0  1  2  3  4
 };
 
 #endif /* Constants_h */
