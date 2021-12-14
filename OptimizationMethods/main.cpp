676fcaffad21806590df1ce50ecbfce77ccb90d9
alageev <alageev01@gmail.com>
2021-12-14 11:05:15 +0300

    main.cpp

--- OptimizationMethods/main.cpp
+++ OptimizationMethods/main.cpp
@@ -7,19 +7,78 @@
 
 #include <iostream>
 #include "Constants.h"
-#include "Functions.h"
+#include "Path.h"
 
 
 int main(int argc, const char * argv[]) {
     
-    {
-        auto startTime = std::chrono::high_resolution_clock::now();
-        const Vector gradientResult = gradientDescent(initialVector);
-        auto endTime = std::chrono::high_resolution_clock::now();
-        auto methodTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+    srand(time(NULL));
+    
+    Path currentPopulation[populationSize];
+    int currentLength = INT_MAX;
+    int currentIteration = 0;
+    
+    while (currentLength > idealLength) {
         
-        std::cout << "gradient descent method: " << function(gradientResult) << " at " << gradientResult;
-        std::cout << ", t = " << methodTime << std::endl;
+        int probabilityAccumulator[populationSize] = { currentPopulation[0].length };
+        for (int i = 1; i < populationSize; i++) {
+            probabilityAccumulator[i] = currentPopulation[i].length + currentPopulation[i - 1].length;
+        }
+        
+        //Finding first to participate in crossingover
+        int const randomChosenFirst = rand() % probabilityAccumulator[populationSize - 1];
+        
+        Path first;
+        int skipIndex = 0;
+        
+        for (int i = 0; i < populationSize; i++) {
+            if (randomChosenFirst < probabilityAccumulator[i]) {
+                first = currentPopulation[i];
+                skipIndex = i;
+                break;
+            }
+        }
+        
+        //Removing first chosen from population
+        probabilityAccumulator[0] = currentPopulation[0].length;
+        
+        //Recalculating probabilities without first
+        for (int i = 1; i < populationSize - 1; i++) {
+            probabilityAccumulator[i] = currentPopulation[i].length + currentPopulation[i - 1].length;
+        }
+        
+        int const randomChosenSecond = rand() % probabilityAccumulator[populationSize - 2];
+        
+        Path second;
+        
+        for (int i = 0; i < populationSize; i++) {
+            if (randomChosenSecond < probabilityAccumulator[i]) {
+                second = currentPopulation[i];
+                break;
+            }
+        }
+        
+        Crossingover crossingover = Crossingover(first, second);
+        
+        Path newPopulation[populationSize + 2];
+        
+        for (int i = 0; i < populationSize - 1; i++) {
+            newPopulation[i] = currentPopulation[i];
+        }
+        
+        newPopulation[populationSize - 1] = first;
+        newPopulation[populationSize    ] = crossingover.first;
+        newPopulation[populationSize + 1] = crossingover.second;
+        
+        std::sort(std::begin(newPopulation), std::end(newPopulation), Path::descending);
+        
+        for (int i = 0; i < populationSize; i++) {
+            currentPopulation[i] = newPopulation[i];
+        }
+        
+        currentLength = currentPopulation[0].length;
+        currentIteration++;
+        std::cout << currentIteration << ": " << currentLength << "\n";
     }
     
     return 0;
